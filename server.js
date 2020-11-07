c4c51735d62572ec0b402fb38f06e2aaeab2fb1c
Deepa17 <54228059+Deepa17@users.noreply.github.com>
2020-11-07 14:34:49 +0530

    Connected to Mongo

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express  = require('express'); //imorting the express module
-
+const connectDB = require('./config/db');
 const app = express(); //variable to express module
 
 app.get('/', (req,res) => res.send('API Running')); //if the request is a success then the response is api running
+
+connectDB();
 
 const PORT = process.env.PORT || 5000; //if there's no env variable set, then the default port is 5000
 
