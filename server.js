5553420b90e54921077ee040c76a9762a00906c3
Deepa17 <54228059+Deepa17@users.noreply.github.com>
2020-11-07 15:23:06 +0530

    Created apis

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const express  = require('express'); //imorting the express module
 const connectDB = require('./config/db');
 const app = express(); //variable to express module
 
+//connect the database
+connectDB();
+
 app.get('/', (req,res) => res.send('API Running')); //if the request is a success then the response is api running
 
-connectDB();
+//Define Routes
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000; //if there's no env variable set, then the default port is 5000
 
